97df3f66263a7793015ffb317f6428f5d357f718
waldorfweb <alex@waldorfshop.eu>
2023-10-31 10:28:27 +0100

    css integrate savelist edit

--- tools/webpack/styles.config.js
+++ tools/webpack/styles.config.js
@@ -3,8 +3,7 @@ const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 const path = require("path");
 const purgecss = require('@fullhuman/postcss-purgecss');
 
-module.exports = env =>
-{
+module.exports = env => {
     env = env || {};
     return {
         name: "styles",
@@ -16,83 +15,91 @@ module.exports = env =>
             shopbuilder: "./resources/scss/shopbuilder.scss"
         },
         module: {
-            rules: [
-                {
-                    test: /.\.scss$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        {
-                            loader: "css-loader",
-                            options: {
-                                url: false,
-                                sourceMap: !env.prod
-                            }
-                        },
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                sourceMap: !env.prod,
-                                plugins: [
-                                    require("autoprefixer")(),
-                                    purgecss({
-                                        content: [
-                                            './src/**/*.php',
-                                            './resources/**/*.vue',
-                                            './resources/**/*.json',
-                                            './resources/**/*.js',
-                                            './resources/**/*.twig',
-                                            './node_modules/Ceres/resources/**/*.twig',
-                                            './node_modules/Ceres/src/**/*.php',
-                                            // './node_modules/bootstrap/**/*.js'
-                                        ],
-                                        safelist: [
-                                            /^category-/,
-                                            /^d-/,
-                                            /^dia-/,
-                                            /^fa-/,
-                                            /^filter-/,
-                                            /^mb-/,
-                                            /^ml-/,
-                                            /^mr-/,
-                                            /^mt-/,
-                                            /^mx-/,
-                                            /^my-/,
-                                            /^m-/,
-                                            /^nav-/,
-                                            /^page-body/,
-                                            /^pb-/,
-                                            /^pl-/,
-                                            /^pr-/,
-                                            /^pt-/,
-                                            /^px-/,
-                                            /^py-/,
-                                            /^p-/,
-                                            /^section-/,
-                                            /^wd-/,
-                                            /^widget_/,
-                                            /breadcrumb/,
-                                            /caption/,
-                                            /col-/,
-                                            /flex/,
-                                            /header/,
-                                            /mainmenu/,
-                                            'btn-form',
-                                            'image-form'
-                                        ]
-                                    })
-                                ]
-                            }
-                        },
-                        {
-                            loader: "sass-loader",
-                            options: {
-                                sourceMap: !env.prod,
-                                outputStyle: env.prod ? "compressed" : "expanded"
-                            }
+            rules: [{
+                test: /.\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                            sourceMap: !env.prod
                         }
-                    ]
-                }
-            ]
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: !env.prod,
+                            plugins: [
+                                require("autoprefixer")(),
+                                purgecss({
+                                    content: [
+                                        './src/**/*.php',
+                                        './resources/**/*.vue',
+                                        './resources/**/*.json',
+                                        './resources/**/*.js',
+                                        './resources/**/*.twig',
+                                        './node_modules/Ceres/resources/**/*.twig',
+                                        './node_modules/Ceres/src/**/*.php',
+                                        // './node_modules/bootstrap/**/*.js'
+                                    ],
+                                    safelist: [
+                                        /^category-/,
+                                        /^d-/,
+                                        /^dia-/,
+                                        /^fa-/,
+                                        /^filter-/,
+                                        /^mb-/,
+                                        /^ml-/,
+                                        /^mr-/,
+                                        /^mt-/,
+                                        /^mx-/,
+                                        /^my-/,
+                                        /^m-/,
+                                        /^nav-/,
+                                        /^page-body/,
+                                        /^pb-/,
+                                        /^pl-/,
+                                        /^pr-/,
+                                        /^pt-/,
+                                        /^px-/,
+                                        /^py-/,
+                                        /^p-/,
+                                        /^section-/,
+                                        /^wd-/,
+                                        /^widget_/,
+                                        /breadcrumb/,
+                                        /caption/,
+                                        /col-/,
+                                        /flex/,
+                                        /header/,
+                                        /mainmenu/,
+                                        'btn-form',
+                                        'image-form',
+                                        /^widget-item-list/,
+                                        /carousel-inner/,
+                                        /carousel-item/,
+                                        /^fa-chevron-/,
+                                        /category-home/,
+                                        /^bs-5/,
+                                        /^footernav-5col/,
+                                        'footernav-info',
+                                        'widget-legal-information'
+
+                                    ]
+                                })
+                            ]
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: !env.prod,
+                            outputStyle: env.prod ? "compressed" : "expanded"
+                        }
+                    }
+                ]
+            }]
         },
         plugins: [
             new FixStyleOnlyEntriesPlugin(),
