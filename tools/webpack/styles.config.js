7deb32f8dcc383126c2f3f8ca4dda647781d20a1
waldorfweb <alex@waldorfshop.eu>
2023-10-31 14:35:55 +0100

    css integrate purge savelist 05 - header does not working fix

--- tools/webpack/styles.config.js
+++ tools/webpack/styles.config.js
@@ -93,6 +93,7 @@ module.exports = env => {
                                         /^catcarousel/,
                                         'initiative-icon',
                                         /^header-first-/,
+                                        /header-first-columns/,
                                         /newsletter-columns/,
                                         /^top-kategorien/
 
