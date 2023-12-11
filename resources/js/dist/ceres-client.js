dc48b3d863ab0b23d96e2e43b7b72a2297583df6
waldorfweb <alex@waldorfshop.eu>
2023-12-11 11:39:17 +0100

    wishlist quantity indicator styling 10

--- resources/js/dist/ceres-client.js
+++ resources/js/dist/ceres-client.js
@@ -5152,7 +5152,7 @@ var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
   return _c("a", {
-    staticClass: "nav-link test",
+    staticClass: "nav-link vue-test",
     attrs: {
       href: _vm.urlWishList,
       rel: "nofollow",
