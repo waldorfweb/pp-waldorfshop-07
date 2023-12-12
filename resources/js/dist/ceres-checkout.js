4f832de7991beb19a05b18aab9525db460e0cd65
waldorfweb <alex@waldorfshop.eu>
2023-12-12 09:30:25 +0100

    wishlist quantity indicator styling 18

--- resources/js/dist/ceres-checkout.js
+++ resources/js/dist/ceres-checkout.js
@@ -8429,7 +8429,6 @@ var render = function render() {
     _c = _vm._self._c;
   return _c("a", {
     staticClass: "nav-link",
-    class: _vm.cssclass,
     attrs: {
       href: _vm.urlWishList,
       rel: "nofollow",
--- resources/js/dist/ceres-client.js
+++ resources/js/dist/ceres-client.js
@@ -5170,7 +5170,6 @@ var render = function render() {
     _c = _vm._self._c;
   return _c("a", {
     staticClass: "nav-link",
-    class: _vm.cssclass,
     attrs: {
       href: _vm.urlWishList,
       rel: "nofollow",
